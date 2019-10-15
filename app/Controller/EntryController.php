f7e908c53cfbcbd00764c55a9fc41edc1ef0f400
Johannes <johannes@przymusinski.de>
2019-10-15 17:27:38 +0200

    prepare createAction for saving the new entry to the database

--- app/Controller/EntryController.php
+++ app/Controller/EntryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Entry;
 use Base;
 
 class EntryController extends BaseController {
@@ -11,12 +12,29 @@ class EntryController extends BaseController {
     }
 
     public function create(Base $f3, array $params) {
+
+        // Load values from form
         $date = $this->f3->get("POST.date");
         $start = $this->f3->get("POST.start");
         $end = $this->f3->get("POST.end");
+        $break = $this->f3->get("POST.break");
         $exp = $this->f3->get("POST.exp");
         $note = $this->f3->get("POST.note");
 
+        // create entry
+        $entry = new Entry();
+        $entry->setDate($date);
+        $entry->setStart($start);
+        $entry->setEnd($end);
+        $entry->setBreak($break);
+        $entry->setExp($exp);
+        $entry->setNote($note);
+
+        // save entry to database
+
+        
+        // reroute to dashboard
+        $this->f3->reroute("/dashboard");
     }
 
     public function editAction(Base $f3, array $params) {
