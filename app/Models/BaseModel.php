7b1f4b4f5498c466875b201d70b96ec1199f487e
Johannes <johannes@przymusinski.de>
2019-10-15 17:44:07 +0200

    create base model

---
+++ app/Models/BaseModel.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Base;
+use Prefab;
+use DB\SQL;
+use DB\SQL\Mapper;
+
+class BaseModel extends Prefab {
+
+    /**
+     * @var Base; 
+     */
+    protected $f3;
+
+    /**
+     * @var \DB\SQL
+     */
+    protected $sql;
+
+    public function __construct()
+    {
+        // Load FatFree instance
+        $this->f3 = Base::instance();
+
+        // Load credentials form database
+        $host = $this->f3->get("DB.HOSTNAME");
+        $port = $this->f3->get("DB.PORT");
+        $name = $this->f3->get("DB.NAME");
+        $user = $this->f3->get("DB.USERNAME");
+        $pass = $this->f3->get("DB.PASSWORD");
+
+        // Initiate the SQL connection
+        $this->sql = new SQL(
+            "mysql:host=$host;dbname=$name;port=$port",
+            $user, $pass
+        );
+    }
+
+    public function getMapper(string $tableName) {
+        $mapper = new Mapper($this->sql, $tableName);
+        return $mapper;
+    }
+
+}
