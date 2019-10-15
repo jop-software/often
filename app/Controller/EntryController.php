6f3516dd383324169346cbc2c1bab1fefcf5d913
Johannes <johannes@przymusinski.de>
2019-10-15 08:47:25 +0200

    create POST handler for creating entries

--- app/Controller/EntryController.php
+++ app/Controller/EntryController.php
@@ -7,7 +7,16 @@ use Base;
 class EntryController extends BaseController {
 
     public function createAction() {
-        echo "create entry";
+        echo $this->render("dashboard/create.html.php");
+    }
+
+    public function create(Base $f3, array $params) {
+        $date = $this->f3->get("POST.date");
+        $start = $this->f3->get("POST.start");
+        $end = $this->f3->get("POST.end");
+        $exp = $this->f3->get("POST.exp");
+        $note = $this->f3->get("POST.note");
+
     }
 
     public function editAction(Base $f3, array $params) {
