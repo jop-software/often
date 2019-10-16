2541ac2fece7928ededc29de46475113d90d414a
Johannes <johannes@przymusinski.de>
2019-10-16 16:32:17 +0200

    add migration configuration

---
+++ migrations.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    "name" => "often",
+    "migration_namespace" => "App\Migrations",
+    "table_name" => "doctrine_migrations",
+    "column_name" => "version",
+    "migrations_directory" => "App/Migrations",
+];
