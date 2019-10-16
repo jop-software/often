23577c32813234c34bc9acba484ca211fba649e6
Johannes <johannes@przymusinski.de>
2019-10-16 16:34:57 +0200

    renamed migration_namespace to migrations_namespace

--- migrations.php
+++ migrations.php
@@ -2,7 +2,7 @@
 
 return [
     "name" => "often",
-    "migration_namespace" => "App\Migrations",
+    "migrations_namespace" => "App\Migrations",
     "table_name" => "doctrine_migrations",
     "column_name" => "version",
     "migrations_directory" => "App/Migrations",
