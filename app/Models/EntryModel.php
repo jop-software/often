03801d82e397040fe0ce5d7327128b5d94b55dc4
Johannes <johannes@przymusinski.de>
2019-10-16 07:08:37 +0200

    save the model to the database

--- app/Models/EntryModel.php
+++ app/Models/EntryModel.php
@@ -7,7 +7,16 @@ use App\Entity\Entry;
 class EntryModel extends BaseModel {
 
     public function save(Entry $entry) {
+        $mapper = $this->getMapper("entry");
 
+        $mapper->date = $entry->getDate();
+        $mapper->start = $entry->getStart();
+        $mapper->end = $entry->getEnd();
+        $mapper->exp = $entry->getExp();
+        $mapper->break = $entry->getBreak();
+        $mapper->note = $entry->getNote();
+
+        $mapper->save();
     }
 
     public function loadById(int $id) {
@@ -19,7 +28,7 @@ class EntryModel extends BaseModel {
     }
 
     public function loadAll() {
-        
+
     }
 
 }
