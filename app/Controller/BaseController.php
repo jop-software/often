3f8f6a495aa7a4cb9b4a91a589a29727e8e02642
Johannes <johannes@przymusinski.de>
2019-10-16 07:26:31 +0200

    add support for parameters in render function

--- app/Controller/BaseController.php
+++ app/Controller/BaseController.php
@@ -23,9 +23,15 @@ class BaseController extends Prefab
     }
 
     /**
-     * Render a template
+     * Render a template with given parameters
      */
-    public function render(string $templateName) {
+    public function render(string $templateName, array $params = []) {
+
+        // Iterate over all given parameters and set them as F3 variables
+        foreach ($params as $key => $value) {
+            $this->f3->set($key, $value);
+        }
+
         $this->f3->set("GUI.template", $templateName);
         return Template::instance()->render("base.html.php");
     }
