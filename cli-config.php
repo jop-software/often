6f59450f17fb394562d9e1c2934b529ea467fecd
Johannes <johannes@przymusinski.de>
2019-10-16 16:13:23 +0200

    initiate DBAL connection

--- cli-config.php
+++ cli-config.php
@@ -1,7 +1,17 @@
 <?php
 
+use Doctrine\DBAL\DriverManager;
+
 require_once "vendor/autoload.php";
 
 // Load user config file (config.ini) to get access to DB credentisla
 $f3 = Base::instance();
 $f3->config("config.ini");
+
+$connection = DriverManager::getConnection([
+    "host" => $f3->get("DB.HOSTNAME"),
+    "user" => $f3->get("DB.USERNAME"),
+    "password" => $f3->get("DB.PASSWORD"),
+    "dbname" => $f3->get("DB.NAME"),
+    "driver" => $f3->get("DB.DRIVER"),
+]);
