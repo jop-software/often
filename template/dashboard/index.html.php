8058b6e6b58158b2d84d7dd31074b3d4243dc131
Johannes <johannes@przymusinski.de>
2019-10-16 07:34:03 +0200

    render buttons with the date of each entry

--- template/dashboard/index.html.php
+++ template/dashboard/index.html.php
@@ -1,1 +1,7 @@
 <h1>Dashboard</h1>
+
+<ul>
+    <repeat group="{{ @entries }}" value="{{ @entry }}">
+        <button><a href="{{ @app.base }}/entry/{{ @entry->getId() }}">{{ @entry->getDate() }}</a></li>
+    </repeat>
+</ul>
