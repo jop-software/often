7297144dfa487fac25ee6d5c5e931caaf90bedbc
Johannes <johannes@przymusinski.de>
2019-10-17 07:26:05 +0200

    render views

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -4,16 +4,35 @@ namespace App\Controller;
 
 class UserController extends BaseController {
 
+    /**
+     * loginAction
+     * GET /login
+     * 
+     * render form, where the user can enter credentials and login
+     */
     public function loginAction() {
-
+        echo $this->render("user/login.html.php");
     }
 
+    /**
+     * registerAction
+     * GET /register
+     * 
+     * render form for the user to register
+     */
     public function registerAction() {
-
+        echo $this->render("user/register.html.php");
     }
 
+    /**
+     * logoutAction
+     * GET /logout
+     * 
+     * render form for the user to logout from their account
+     * todo: do we need this or should we just log the user out on GET /logout?
+     */
     public function logoutAction() {
-        
+        echo $this->render("user/logout.html.php");
     }
 
 }
