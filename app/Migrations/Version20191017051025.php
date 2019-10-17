d5e28a8e3b667a0dbd7783a0722a319d1f0e27c8
Johannes <johannes@przymusinski.de>
2019-10-17 07:19:01 +0200

    [create-migration] add userid to entry

---
+++ app/Migrations/Version20191017051025.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191017051025 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $table = $schema->getTable("entry");
+
+        $table->addColumn("userid", "integer");
+
+        $table->addForeignKeyConstraint("user", ["userid"], ["ID"], [], "entry_userid");
+
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $table = $schema->getTable("entry");
+        $table->dropColumn("userid");
+        $table->removeForeignKey("entry_userid");
+
+    }
+}
