002bc782bbd90af2b2510e3ee106d9af9fa2958a
Johannes <johannes@przymusinski.de>
2019-10-17 07:21:00 +0200

    [controller] create usercontroller with basic function headers

---
+++ app/Controller/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+class UserController extends BaseController {
+
+    public function loginAction() {
+
+    }
+
+    public function registerAction() {
+
+    }
+
+    public function logoutAction() {
+        
+    }
+
+}
