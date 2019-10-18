64c64422f649094d0477e8debcbe310c71c3d714
Johannes <johannes@przymusinski.de>
2019-10-18 11:30:15 +0200

    create login form

--- template/user/login.html.php
+++ template/user/login.html.php
@@ -1,1 +1,23 @@
 <include href="utils/errors.html.php" />
+
+<style>
+.form-group {
+    display: block;
+}    
+</style>
+
+<h1>Login</h1>
+
+<form action="login" method="POST">
+    <div class="form-group">
+        <label for="username">Benutzername</label>
+        <input type="text" name="username" id="username">
+    </div>
+    <div class="form-group">
+        <label for="password">Passwort</label>
+        <input type="password" name="password" id="password">
+    </div>
+    <div class="form-group">
+        <input type="submit" value="Login">
+    </div>
+</form>
