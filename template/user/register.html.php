6119c93c14161b60f1640a935c5c661d18e81a38
Johannes <johannes@przymusinski.de>
2019-10-18 11:30:29 +0200

    create register form

---
+++ template/user/register.html.php
@@ -0,0 +1,23 @@
+<include href="utils/errors.html.php" />
+
+<style>
+.form-group {
+    display: block;
+}    
+</style>
+
+<h1>Register</h1>
+
+<form action="register" method="POST">
+    <div class="form-group">
+        <label for="username">Benutzername</label>
+        <input type="text" name="username" id="username">
+    </div>
+    <div class="form-group">
+        <label for="password">Passwort</label>
+        <input type="password" name="password" id="password">
+    </div>
+    <div class="form-group">
+        <input type="submit" value="Register">
+    </div>
+</form>
