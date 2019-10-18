6226bb13750fe67e9c2389d281c4768a9cdaf32d
Johannes <johannes@przymusinski.de>
2019-10-18 09:41:52 +0200

    add error reporting

--- app/Controller/BaseController.php
+++ app/Controller/BaseController.php
@@ -15,6 +15,11 @@ class BaseController extends Prefab
     protected $f3;
 
     /**
+     * @var string[]
+     */
+    protected $errors;
+
+    /**
      * BaseController constructor
      */
     public function __construct()
@@ -23,14 +28,35 @@ class BaseController extends Prefab
     }
 
     /**
+     * call if you got an error
+     * -> errors will get to the view
+     */
+    public function error(string $msg) {
+        array_push($this->errors, $msg);
+    }
+
+    /**
+     * clear the error array
+     */
+    public function clearErrors() {
+        $this->errors = [];
+    }
+
+    /**
      * Render a template with given parameters
      */
-    public function render(string $templateName, array $params = []) {
+    public function render(string $templateName, array $params = [], bool $clearErrors = true) {
 
         // Iterate over all given parameters and set them as F3 variables
         foreach ($params as $key => $value) {
             $this->f3->set($key, $value);
         }
+
+        // pass the error array to the view
+        $this->f3->set("errors", $this->errors);
+
+        // clear the errors if the flag is set
+        if ($clearErrors) $this->clearErrors();
 
         $this->f3->set("GUI.template", $templateName);
         return Template::instance()->render("base.html.php");
