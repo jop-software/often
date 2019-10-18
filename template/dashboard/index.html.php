bfc5b1bbd7da869df8fa5236e964b4f58323c6dc
Johannes <johannes@przymusinski.de>
2019-10-18 12:59:49 +0200

    add user auth buttons (register login logout) to dashboard. show only available buttons to user

--- template/dashboard/index.html.php
+++ template/dashboard/index.html.php
@@ -11,3 +11,11 @@
 </ul>
 
 <a href="{{ @app.base }}/entry/create"><button>New</button></a>
+<check if="{{ @SESSION.userid }}">
+    <true>
+        <a href="{{ @app.base }}/login"><button>Login</button></a>
+    </true>
+    <false>
+        <a href="{{ @app.base }}/register"><button>Register</button></a>
+    </false>
+</check>
