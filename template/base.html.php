a35372b9c7f78b83da161de021bd7e957f75616b
Johannes Przymusinski <johannes@przymusinski.de>
2019-10-19 15:37:33 +0200

    update version number to 1.0.0

--- template/base.html.php
+++ template/base.html.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>often v0.0.1 by Johannes Przymusinski</title>
+        <title>often {{ @app.version }} by Johannes Przymusinski</title>
     </head>
     <body>
         <include href="utils/errors.html.php" />
