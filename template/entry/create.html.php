a997d5aa91f2ddc0d8ae927b58f1128721b55002
Johannes <johannes@przymusinski.de>
2019-10-20 08:15:53 +0200

    updated entry/create form for use language files

--- template/entry/create.html.php
+++ template/entry/create.html.php
@@ -1,29 +1,29 @@
-<h1><a href="{{@app.base}}/dashboard"><-</a>Create Entry</h1>
+<h1><a href="{{@app.base}}/dashboard"><-</a>{{ @language.entry.create }}</h1>
 
 <form action="{{ @app.base }}/createEntry" method="POST">
     <div class="form-group">
-        <label for="date">Date:</label>
+        <label for="date">{{ @language.entry.date }}:</label>
         <input type="date" name="date" id="date">
     </div>
     <div class="form-group">
-        <label for="start">Start:</label>
+        <label for="start">{{ @language.entry.start }}:</label>
         <input type="time" name="start" id="start">
     </div>
     <div class="form-group">
-        <label for="end">End:</label>
+        <label for="end">{{ @language.entry.end }}:</label>
         <input type="time" name="end" id="end">
     </div>
     <div class="form-group">
-        <label for="break">Break:</label>
+        <label for="break">{{ @language.entry.break }}:</label>
         <input type="time" name="break" id="break">
     </div>
     <div class="form-group">
-        <label for="exp">Expectation:</label>
+        <label for="exp">{{ @language.entry.exp }}:</label>
         <input type="time" name="exp" id="exp">
     </div>
     <div class="form-group">
-        <label for="note">Note:</label>
+        <label for="note">{{ @language.entry.note }}:</label>
         <textarea type="text" name="note" id="note"></textarea>
     </div>
-    <input type="submit" value="create">
+    <input type="submit" value="{{ @language.entry.submit }}">
 </form>
