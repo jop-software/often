8e0ded68836a4047e65797d34be892d63413a07e
Johannes <johannes@przymusinski.de>
2019-10-20 08:17:08 +0200

    udpdated entry/delete form to use language files

--- template/entry/delete.html.php
+++ template/entry/delete.html.php
@@ -1,8 +1,8 @@
-<h1 style="color: red">DELETE</h1>
-<h2>Are you sure, you want to delete this entry?</h2>
+<h1 style="color: red">{{ @language.entry.delete.title }}</h1>
+<h2>{{ @language.entry.delete.info }}</h2>
 
 <form action="{{ @app.base }}/deleteEntry" method="post">
     <input type="hidden" name="id" value="{{ @entry->getId() }}">
-    <input type="submit" name="delete" value="YES">
+    <input type="submit" name="delete" value="{{ @language.entry.delete.submit }}">
 </form>
-<a href="{{ @app.base }}/entry/{{ @entry->getId() }}"><button>NO</button></a>
+<a href="{{ @app.base }}/entry/{{ @entry->getId() }}"><button>{{ @language.entry.delete.abort }}</button></a>
