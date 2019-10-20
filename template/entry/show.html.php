a5b3341ebda9d29a94fadfc6869f92fcc2210411
Johannes <johannes@przymusinski.de>
2019-10-20 08:20:58 +0200

    updated entry/show form to use the language files

--- template/entry/show.html.php
+++ template/entry/show.html.php
@@ -3,42 +3,42 @@
 <table>
     <tbody>
         <tr>
-            <td>Date</td>
+            <td>{{ @language.entry.date }}</td>
             <td>{{ @entry->getDate() }}</td>
         </tr>
         <tr>
-            <td>Start</td>
+            <td>{{ @language.entry.start }}</td>
             <td>{{ @entry->getStart() }}</td>
         </tr>
         <tr>
-            <td>End</td>
+            <td>{{ @language.entry.end }}</td>
             <td>{{ @entry->getEnd() }}</td>
         </tr>
         <tr>
-            <td>Diff</td>
+            <td>{{ @language.entry.diff }}</td>
             <td>
                 {~ @diff = @entry->getWorktimeDifference() ~}
                 {{ @diff[0] }}:{{ @diff[1] }}
             </td>
         </tr>
         <tr>
-            <td>Break</td>
+            <td>{{ @language.entry.break }}</td>
             <td>{{ @entry->getBreak() }}</td>
         </tr>
         <tr>
-            <td>Expectation</td>
+            <td>{{ @language.entry.exp }}</td>
             <td>{{ @entry->getExp() }}</td>
         </tr>
         <tr>
-            <td>Worked</td>
+            <td>{{ @language.entry.worked }}</td>
             <td>{{ @entry->getWorkedTime() }}</td>
         </tr>
         <tr>
-            <td>Note</td>
+            <td>{{ @language.entry.note }}</td>
             <td>{{ @entry->getNote() }}</td>
         </tr>
     </tbody>
 </table>
 
-<a href="{{ @app.base }}/entry/{{ @entry->getId() }}/delete"><button>delete</button></a>
-<a href="{{ @app.base }}/entry/{{ @entry->getId() }}/edit"><button>edit</button></a>
+<a href="{{ @app.base }}/entry/{{ @entry->getId() }}/delete"><button>{{ @language.entry.show.delete }}</button></a>
+<a href="{{ @app.base }}/entry/{{ @entry->getId() }}/edit"><button>{{ @language.entry.show.edit }}</button></a>
