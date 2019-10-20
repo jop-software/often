af830d9497422d22f3c8b8a9333fb326e972eaf5
Johannes <johannes@przymusinski.de>
2019-10-20 07:53:58 +0200

    load the english translation by default

--- index.php
+++ index.php
@@ -5,6 +5,11 @@ require_once "vendor/autoload.php";
 $f3 = Base::instance();
 
 $f3->config("config.ini");
+
 $f3->config("app/config/config.cfg");
+
+// load the english translation as fallback
+// we load the config file of the user "over" the english one
+$f3->config("app/config/languages/en_us.cfg");
 
 $f3->run();
