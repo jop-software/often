31c1da9c22161cfbb999d5c4dfe03081f0eff39d
Johannes <johannes@przymusinski.de>
2019-10-20 08:23:05 +0200

    updated entry/edit form to use language files

--- template/entry/edit.html.php
+++ template/entry/edit.html.php
@@ -1,38 +1,38 @@
-<h1> <a href="{{@app.base}}/dashboard"><-</a> Edit {{ @entry->getDate() }}</h1>
+<h1> <a href="{{@app.base}}/dashboard"><-</a> {{ @language.entry.show.edit }} {{ @entry->getDate() }}</h1>
 
 <form action="{{ @app.base }}/editEntry" method="POST">
     <input type="hidden" name="id" value="{{ @entry->getId() }}">
     <table>
         <tbody>
             <tr>
-                <td>ID</td>
+                <td>{{ @language.entry.id }}</td>
                 <td>{{ @entry->getId() }}</td>
             </tr>
             <tr>
-                <td>Date</td>
+                <td>{{ @language.entry.date }}</td>
                 <td><input type="date" name="date" value="{{ @entry->getDate() }}"></td>
             </tr>
             <tr>
-                <td>Start</td>
+                <td>{{ @language.entry.start }}</td>
                 <td><input type="time" name="start" value="{{ @entry->getStart() }}"></td>
             </tr>
             <tr>
-                <td>End</td>
+                <td>{{ @language.entry.end }}</td>
                 <td><input type="time" name="end" value="{{ @entry->getEnd() }}"></td>
             </tr>
             <tr>
-                <td>Break</td>
+                <td>{{ @language.entry.break }}</td>
                 <td><input type="time" name="break" value="{{ @entry->getBreak() }}"></td>
             </tr>
             <tr>
-                <td>Expectation</td>
+                <td>{{ @language.entry.exp }}</td>
                 <td><input type="time" name="exp" value="{{ @entry->getExp() }}"></td>
             </tr>
             <tr>
-                <td>Note</td>
+                <td>{{ @language.entry.note }}</td>
                 <td><textarea name="note">{{ @entry->getNote() }}</textarea></td>
             </tr>
         </tbody>
     </table>
-    <input type="submit" name="submit" value="save">
+    <input type="submit" name="submit" value="{{ @language.entry.show.edit }}">
 </form>
