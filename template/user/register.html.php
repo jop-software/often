7fbdd3656d866ff6630614d4150940d93527d785
Johannes <johannes@przymusinski.de>
2019-10-20 08:26:41 +0200

    updated user/register form to use language files

--- template/user/register.html.php
+++ template/user/register.html.php
@@ -4,25 +4,25 @@
 }    
 </style>
 
-<h1>Register</h1>
+<h1>{{ @language.register.title }}</h1>
 
 <form action="{{ @app.base }}/register" method="POST">
     <div class="form-group">
-        <label for="username">Benutzername</label>
+        <label for="username">{{ @language.register.form.username }}</label>
         <input type="text" name="username" id="username">
     </div>
     <div class="form-group">
-        <label for="password">Passwort</label>
+        <label for="password">{{ @language.register.form.password }}</label>
         <input type="password" name="password" id="password">
     </div>
     <div class="form-group">
-        <label for="language">Language</label>
+        <label for="language">{{ @language.register.form.language }}</label>
         <select name="language" id="language">
             <option value="de_de">Deutsch</option>
             <option value="en_us">English</option>
         </select>
     </div>
     <div class="form-group">
-        <input type="submit" value="Register">
+        <input type="submit" value="{{ @language.register.form.submit }}">
     </div>
 </form>
