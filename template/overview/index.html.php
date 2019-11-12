3efb0fd99a43130368e2f9abcc34f0b0efe37a8b
Johannes <johannes@przymusinski.de>
2019-11-12 15:04:43 +0100

    show all months with year information

--- template/overview/index.html.php
+++ template/overview/index.html.php
@@ -1,1 +1,9 @@
 <h1>{{ @language.overview.title }}</h1>
+
+<ul>
+    <repeat group="{{ @months }}" value="{{ @item }}">
+        {~ @month = @item["month"] ~}
+        {~ @year = @item["year"] ~}
+        <li><a href="{{ @app.base }}/overview/{{ @year }}/{{ @month }}">{{ @month }} {{ @year }}</a></li>    
+    </repeat>
+</ul>
