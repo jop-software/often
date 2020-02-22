5a6793ac3c8fa786586b487eb7d487d26622a145
Johannes Przymusinski <johannes@przymusinski.de>
2020-02-22 17:52:53 +0000

    rename app/ to App/

--- index.php
+++ index.php
@@ -6,10 +6,10 @@ $f3 = Base::instance();
 
 $f3->config("config.ini");
 
-$f3->config("app/config/config.cfg");
+$f3->config("App/config/config.cfg");
 
 // load the english translation as fallback
 // we load the config file of the user "over" the english one
-$f3->config("app/config/languages/en_us.cfg");
+$f3->config("App/config/languages/en_us.cfg");
 
 $f3->run();
