5f03a0f4c8ccd0c320b2a4f38dac5825898c3029
Johannes Przymusinski <johannes@przymusinski.de>
2020-02-24 22:58:41 +0100

    uodate git-setup script

--- scripts/setup-git.sh
+++ scripts/setup-git.sh
@@ -2,4 +2,4 @@ git remote remove origin
 
 git remote add origin http://git.johannesprzymusinski.de/Johannes/often.git
 
-git remote set-url --push --add origin git@github.com:cngJo/often.git
+git config --add remote.origin.url git@github.com:cngJo/often.git
