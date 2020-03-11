1b080d4b1afa0cfd78430ce1870372b95ab0770b
Johannes Przymusinski <johannes@przymusinski.de>
2020-03-11 17:24:20 +0100

    implement SessionWrapper class for better interaction with the session

---
+++ App/Core/SessionWrapper.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Core;
+
+use Base;
+
+class SessionWrapper 
+{
+
+    /**
+     * Add a message to the session
+     * messges will get passed to twig during the next render process and then deleted
+     * 
+     * @param string $message
+     * @param string $type
+     * @return void
+     */
+    public static function addMessage(string $message, string $type) : void
+    {
+        // load all messages currently stored in the session or initialize an empty array
+        $sessionMessages = Base::instance()->get("SESSION.MESSAGES") || array();
+
+        // create and append the message object
+        $sessionMessages[] = [
+            "message" => $message,
+            "type" => $type
+        ];
+
+
+        // write the errors back to the session
+        Base::instance()->set("SESSION.MESSAGES", $sessionMessages);
+    }
+
+    /**
+     * Get all messages from the session
+     * returns null if there are no messages
+     * 
+     * @return null|array
+     */
+    public static function getMessages() : ?array
+    {
+        return Base::instance()->get("SESSION.MESSAGES");
+    }
+
+    public static function clearMessages() : void
+    {
+        // write null to the messages array in the session
+        Base::instance()->set("SESSION.MESSAGES", null);
+    }
+
+}
