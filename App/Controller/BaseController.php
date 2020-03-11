9c291244745a63c9c8d5e203f6fcb7e5b9706417
Johannes Przymusinski <johannes@przymusinski.de>
2020-03-11 20:46:16 +0100

    delete render() function from BaseController

--- App/Controller/BaseController.php
+++ App/Controller/BaseController.php
@@ -76,31 +76,6 @@ class BaseController extends Prefab
     }
 
     /**
-     * Render a template with given parameters
-     *
-     * @deprecated
-     */
-    public function render(string $templateName, array $params = [], bool $clearErrors = true) {
-
-        // Iterate over all given parameters and set them as F3 variables
-        foreach ($params as $key => $value) {
-            $this->f3->set($key, $value);
-        }
-
-        $this->f3->set("GUI.template", $templateName);
-        $template = Template::instance()->render("base.html.php");
-
-        // we have to clear the errors after the render, but before the end
-        // of this function
-
-        // check if the $clearErrors flag is set
-        // if so, call the clearErrors function
-        // if ($clearErrors) $this->clearErrors();
-
-        return $template;
-    }
-
-    /**
      * Render a TWIG-Template
      *
      * @param string $name name of the template
