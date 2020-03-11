60e4af677a3c819abb6783fdef50852c57ec35e7
Johannes Przymusinski <johannes@przymusinski.de>
2020-03-11 20:44:03 +0100

    remove template folder, home of the old F3 Templates

--- template/entry/delete.html.php
+++
@@ -1,8 +0,0 @@
-<h1 style="color: red">{{ @language.entry.delete.title }}</h1>
-<h2>{{ @language.entry.delete.info }}</h2>
-
-<form action="{{ @app.base }}/deleteEntry" method="post">
-    <input type="hidden" name="id" value="{{ @entry->getId() }}">
-    <input type="submit" name="delete" value="{{ @language.entry.delete.submit }}">
-</form>
-<a href="{{ @app.base }}/entry/{{ @entry->getId() }}"><button>{{ @language.entry.delete.abort }}</button></a>
--- template/overview/index.html.php
+++
@@ -1,9 +0,0 @@
-<h1>{{ @language.overview.title }}</h1>
-
-<ul>
-    <repeat group="{{ @months }}" value="{{ @item }}">
-        {~ @month = @item["month"] ~}
-        {~ @year = @item["year"] ~}
-        <li><a href="{{ @app.base }}/overview/{{ @year }}/{{ @month }}">{{ @month }} {{ @year }}</a></li>    
-    </repeat>
-</ul>
