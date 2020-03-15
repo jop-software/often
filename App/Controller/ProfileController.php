234b8a352c3803b9b51fb08b0fa13552c41e9d8f
Johannes <johannes@przymusinski.de>
2020-03-15 20:01:41 +0100

    implement updateProfileDate()

--- App/Controller/ProfileController.php
+++ App/Controller/ProfileController.php
@@ -22,4 +22,30 @@ class ProfileController extends BaseController {
         ]);
     }
 
+    /**
+     * POST /udpate/user/data
+     * 
+     * interal update from the user data
+     */
+    public function updateProfileData()
+    {
+        $form = $this->f3->clean($this->f3->get("POST"));
+        $userId = SessionWrapper::getUserId();
+
+        $userModel = new UserModel();
+        $user = $userModel->getUserFromId($userId);
+
+        // check if the username does exist in the database before setting it
+        if (!$userModel->doesUsernameExist($username = $form["username"])) {
+
+            // set the new username in the Entity and update in the database
+            $user->setUsername($username);
+            $userModel->updateUser($user);
+        } else {
+            $this->error("Dieser Benutzername ist bereits vergeben");
+        }
+
+        $this->f3->reroute("/profile");
+    }
+
 }
