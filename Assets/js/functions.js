f12c4bfba2d11e74e7d4149b9af07f78335a4c19
Johannes <johannes@przymusinski.de>
2020-04-02 12:02:02 +0200

    implement Popup as warning when try to delete all entries

--- Assets/js/functions.js
+++ Assets/js/functions.js
@@ -15,3 +15,21 @@ function deleteEntry(id) {
         }
     })
 }
+
+function deleteEntryAll() {
+    Swal.fire({
+        title: `ALLE Einträge Löschen`,
+        text: "Sicher, das du ALLE Einträge löschen möchtest?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#d33',
+        cancelButtonColor: '#3085d6',
+        confirmButtonText: 'Löschen',
+        cancelButtonText: 'Abbrechen'
+    }).then((result) => {
+        if (result.value) {
+            let form = document.querySelector("#delete_entry_all");
+            form.submit();
+        }
+    })
+}
